cd854a6f81c8e9eeaa897eace0472812adbf9c8f
GromoverGetsM <miroshkin.kirill09@gmail.com>
2025-01-06 18:31:09 +0300

    fix database

--- src/main/java/ru/rstudios/creative/utils/DatabaseUtil.java
+++ src/main/java/ru/rstudios/creative/utils/DatabaseUtil.java
@@ -40,6 +40,23 @@ public class DatabaseUtil {
             CREATE TABLE IF NOT EXISTS players (
                 id INT PRIMARY KEY AUTO_INCREMENT,
                 player_name VARCHAR(100) NOT NULL,
+                player_locale VARCHAR(100)
+            );
+        """;
+        String createPlotTable = """
+            CREATE TABLE IF NOT EXISTS plots (
+                id INT PRIMARY KEY,
+                plot_name VARCHAR(100) NOT NULL,
+                custom_id VARCHAR(100),
+                owner_name VARCHAR(100) NOT NULL,
+                openedState BOOLEAN,
+                icon VARCHAR(100),
+                icon_name VARCHAR(100),
+                icon_lore VARCHAR(500),
+                cost BIGINT,
+                environment VARCHAR(100),
+                generation VARCHAR(100),
+                gen_structures BOOLEAN
                 team VARCHAR(100),
                 lvl INT,
                 exp INT,
@@ -58,6 +75,7 @@ public class DatabaseUtil {
         Statement pstmt = conn.createStatement();
 
         pstmt.execute(createPlayerTable);
+        pstmt.execute(createPlotTable);
     }
 
     public static void insertValue (String tableName, String columnName, Object value) {
